dfad8cece139e09da50489d271c7c172d9999d13
Ivor Myers <imyers@ucsd.edu>
2024-05-07 23:47:29 -0700

    unit tests added

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -18,11 +18,74 @@ test('test isPhoneNumber (831) 250-3633', () => {
   expect(isPhoneNumber("(831) 250-3633")).toBe(true);
 });
 
-test('test isPhoneNumber: 831-250-363', () => {
+test('test isPhoneNumber: 831250-363', () => {
   expect(isPhoneNumber("831250-363")).toBe(false);
 });
 
-test('test isPhoneNumber (831250-3633', () => {
+test('test isPhoneNumber 831250363', () => {
   expect(isPhoneNumber("8312503633")).toBe(false);
 });
 
+test('test isEmail: raw@gmail.com',() =>{
+  expect(isEmail("raw@gmail.com")).toBe(true);
+});
+
+test('test isEmail: a@yahoo.net',() =>{
+  expect(isEmail("a@yahoo.net")).toBe(true);
+});
+
+test('test isEmail: @yahoo.net',() =>{
+  expect(isEmail("@yahoo.net")).toBe(false);
+});
+
+test('test isEmail: hellogmail.com',() =>{
+  expect(isEmail("hellogmail.com")).toBe(false);
+});
+
+test('test isStrongPassword:YEAYUH',() =>{
+  expect(isStrongPassword("YEAYUH")).toBe(true);
+});
+
+test('test isStrongPassword: OOOYEAH123',() =>{
+  expect(isStrongPassword("OOOYEAH123")).toBe(true);
+});
+
+test('test isStrongPassword: a',() =>{
+  expect(isStrongPassword("a")).toBe(false);
+});
+
+test('test isStrongPassword:12',() =>{
+  expect(isStrongPassword("12")).toBe(false);
+});
+
+test('test isDate: 01/01/2001',() =>{
+  expect(isDate("01/01/2001")).toBe(true);
+});
+
+test('test isDate: 1/1/2001',() =>{
+  expect(isDate("1/1/2001")).toBe(true);
+});
+
+test('test isDate: 01/01/01',() =>{
+  expect(isDate("01/01/01")).toBe(false);
+});
+
+test('test isDate: /01/2001',() =>{
+  expect(isDate("/01/2001")).toBe(false);
+});
+
+test('test isHexColor:#123',() =>{
+  expect(isHexColor("#123")).toBe(true);
+});
+
+test('test isHexColor:112233',() =>{
+  expect(isHexColor("112233")).toBe(true);
+});
+
+test('test isHexColor:23',() =>{
+  expect(isHexColor("23")).toBe(false);
+});
+
+test('test isHexColor:#1231234',() =>{
+  expect(isHexColor("#1231234")).toBe(false);
+});

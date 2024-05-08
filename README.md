6edb158c07ec54225b453f699e412a2c62a79ead
Ivor Myers <imyers@ucsd.edu>
2024-05-07 23:27:06 -0700

    some small changes

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Lab 5 - Starter
 Ivor Myers
+
+Link to [expose.html](https://ivormyers.github.io/Lab5_Starter/expose.html).
+
+
+
+Check Understanding: 
+1. I would not use a unit test, as there are a lot of external factors that could also effect the message going through or not--such as bad internet--which might not be possible to emulate in a unit test. 
+2. I would use a unit test on the max lenght feature, as it is an isolated function that can be easily checked with a unit test. 
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -9,3 +9,20 @@ import {
 } from '../code-to-unit-test/unit-test-me';
 
 // TODO - Part 2
+
+test('test isPhoneNumber: 831-250-3633', () => {
+  expect(isPhoneNumber("831-250-3633")).toBe(true);
+});
+
+test('test isPhoneNumber (831) 250-3633', () => {
+  expect(isPhoneNumber("(831) 250-3633")).toBe(true);
+});
+
+test('test isPhoneNumber: 831-250-363', () => {
+  expect(isPhoneNumber("831250-363")).toBe(false);
+});
+
+test('test isPhoneNumber (831250-3633', () => {
+  expect(isPhoneNumber("8312503633")).toBe(false);
+});
+
